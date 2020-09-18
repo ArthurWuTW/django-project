a740fd9967ea72892d4d9612a1c442e8d9f312b7
ArthurWuTW <lion811004@gmail.com>
2020-09-19 07:59:57 +0800

    Create app's first view function

--- monitor_app/views.py
+++ monitor_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def dashboard(request):
+
+    context = {
+
+    }
+
+    return render(request, 'template_dashboard/dashboard.html', context)
