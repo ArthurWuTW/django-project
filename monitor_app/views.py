e69cdf0886a1eb8b515c6612b2b8cdeeef85fea0
ArthurWuTW <lion811004@gmail.com>
2020-09-22 06:08:53 +0800

    Initialize temperature views

--- monitor_app/views.py
+++ monitor_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def dashboard(request):
-
     context = {
-
     }
-
     return render(request, 'template_dashboard/dashboard.html', context)
+
+def temperature(request, temp):
+
+    #write into database
+    
+
+    return HttpResponse('')
