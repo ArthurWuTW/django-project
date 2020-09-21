9315bf26afef0662eb61cf307c0ee966990a7807
ArthurWuTW <lion811004@gmail.com>
2020-09-22 06:08:27 +0800

    Add functionality <float:XXX> in urls.py

--- monitor_app/urls.py
+++ monitor_app/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path, include
 from . import views
 
+from . import converters
+from django.urls import register_converter
+register_converter(converters.FloatConverter, 'float')
+
 urlpatterns = [
-    path('',views.dashboard,name='dashboard')
+    path('',
+        views.dashboard,
+        name='dashboard'),
+    path('temperature/<float:temp>',
+        views.temperature,
+        name='temperature')
 ]
