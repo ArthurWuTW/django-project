09b6b136002d7944751dab7cc864673938675e4c
ArthurWuTW <lion811004@gmail.com>
2020-09-22 10:49:46 +0800

    Show data in dashboard

--- monitor_app/views.py
+++ monitor_app/views.py
@@ -3,13 +3,29 @@ from django.http import HttpResponse
 from .models import *
 from .forms import *
 from datetime import datetime, timedelta
+from .view_utils import *
+import json
 
 # Create your views here.
 def dashboard(request):
 
+    temps = Temperature.objects.all()
+    humids = Humidity.objects.all()
+
     timezone_hour_offset = 8
 
+    temp_list = list()
+    for temp in temps:
+        temp_list.append({
+            'timestamp': (temp.time + timedelta(hours=8)).strftime('%Y-%m-%d %H:%M'),
+            'temperature': temp.temperature
+        })
+
+    print(temp_list)
+
+
     context = {
+        'temp_list': temp_list
     }
     return render(request, 'template_dashboard/dashboard.html', context)
 
