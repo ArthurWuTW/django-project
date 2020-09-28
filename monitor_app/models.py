30c254bdc759d1ff62a99e8ba8cd6dd4e9836ea0
ArthurWuTW <lion811004@gmail.com>
2020-09-28 08:58:58 +0800

    Add model GrowthRate

--- monitor_app/models.py
+++ monitor_app/models.py
@@ -50,3 +50,16 @@ class TimePrice(models.Model):
 
     class Meta:
         verbose_name_plural = 'TimePrice'
+
+class GrowthRate(models.Model):
+    time = models.DateTimeField(null=True)
+    plant_id = models.IntegerField(blank=True, null=True)
+    rate = models.FloatField(null=True,
+                              blank=True,
+                              default=None)
+
+    def __str__(self):
+        return str(self.time)+"_"+str(self.plant_id)
+
+    class Meta:
+        verbose_name_plural = 'GrowthRate'
