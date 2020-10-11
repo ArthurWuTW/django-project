9f59331addceffe3d92fda1dbbe89eb5a550a202
ArthurWuTW <lion811004@gmail.com>
2020-10-11 10:34:10 +0800

    Add account(easiest way)

--- monitor_app/views.py
+++ monitor_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as auth_login
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
@@ -170,22 +171,33 @@ def register(request):
     if(request.method == 'POST'):
         email = request.POST.get('email', '')
         password = request.POST.get('password', '')
+        repeat_password = request.POST.get('repeat_password', '')
+
+        if(password!=repeat_password):
+            context = {
+                'status_message': 'Repeat Password is not the same as Password!'
+
+            }
+            return render(request, 'template_dashboard/register.html', context)
 
         print("adf",email, password)
 
-    # if POST
-    #    email
-    #    username
-    #    password
-    #    if email is registered
-    #         render html with message
-    #    else
-    #         create user
-    #         redirect to login page
+        if not User.objects.filter(username=email).exists():
+            user = User.objects.create_user(username=email,
+                                            email=email,
+                                            password=password)
+            user.save()
+
+            return redirect('/login')
+        else:
+            context = {
+                'status_message': 'The email already exists!'
+            }
+            return render(request, 'template_dashboard/register.html', context)
 
 
     context = {
-
+        'status_message': 'Create an Account'
     }
     return render(request, 'template_dashboard/register.html', context)
 
