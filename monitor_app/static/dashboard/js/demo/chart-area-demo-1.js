ea1de46da8c1aed74c0f35a25e3fd98d440e14a1
ArthurWuTW <lion811004@gmail.com>
2020-10-13 11:06:21 +0800

    Visualization: temperature

--- monitor_app/static/dashboard/js/demo/chart-area-demo-1.js
+++ monitor_app/static/dashboard/js/demo/chart-area-demo-1.js
@@ -1,55 +1,57 @@
 // Area Chart Example
 var ctx = document.getElementById("myAreaChart_1");
 var chart = new Chart(ctx, {
-    type:'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-    options:{
-      maintainAspectRatio:false,
-      legend:{
-        display:false
-      },
-      title:{},
-      scales:{
-        xAxes:[
-          {
-            gridLines:{
-              color:'rgb(234, 236, 244)',
-              zeroLineColor:'rgb(234, 236, 244)',
-              drawBorder:false,
-              drawTicks:false,
-              borderDash:[2],
-              zeroLineBorderDash:[2],
-              drawOnChartArea:false},
-              ticks:{
-                fontColor:'#858796',
-                padding:20
-              }
+        type:'line',
+        data: {
+          labels: temp_data['timestamp_array'],
+          datasets: [{
+            label: 'Temperature',
+            data: temp_data['temp_array'],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+            ],
+            borderWidth: 1
+          }]
+        },
+        options:{
+          maintainAspectRatio:false,
+          legend:{
+            display:false
+          },
+          title:{},
+          scales:{
+            xAxes:[
+              {
+                  ticks:{
+                    fontColor:'#858796',
+                    padding:20,
+                    autoSkip: true,
+                    maxTicksLimit: 20,
+                  }
+                }
+              ],
+              yAxes:[
+                {
+                  gridLines:{
+                    color:'rgb(234, 236, 244)',
+                    zeroLineColor:'rgb(234, 236, 244)',
+                    drawBorder:false,
+                    drawTicks:false,
+                    borderDash:[2],
+                    zeroLineBorderDash:[2]
+                  },
+                  ticks:{
+                    fontColor:'#858796',
+                    padding:5,
+                    suggestedMin: 20,
+                    suggestedMax: 34
+                  }
+                }
+              ]
             }
-          ],
-          yAxes:[
-            {
-              gridLines:{
-                color:'rgb(234, 236, 244)',
-                zeroLineColor:'rgb(234, 236, 244)',
-                drawBorder:false,
-                drawTicks:false,
-                borderDash:[2],
-                zeroLineBorderDash:[2]
-              },
-              ticks:{
-                fontColor:'#858796',
-                padding:20
-              }
-            }
-          ]
-        }
-      }
+          }
 });
