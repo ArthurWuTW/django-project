53c29c7f710a00acc57b1e2606f70e81710b89f1
ArthurWuTW <lion811004@gmail.com>
2020-10-28 22:56:59 +0800

    forgot_password:replace get with filter, sys die if object is not match

--- monitor_app/views_collection/forgot_password.py
+++ monitor_app/views_collection/forgot_password.py
@@ -20,41 +20,38 @@ def forgot_password(request):
     if(request.method == 'POST'):
         email = request.POST.get('email', '')
         print("email", email)
-        associated_user = User.objects.get(email=email)
-        print("associated_user", associated_user)
-        # WARNING if we already know there is only one onject matches our jquery
-        # we can use get - which will throw a DoesNotExistException
-        subject = "Password Reset Requested"
-        email_template_name = "../templates/password_reset_email_template.txt"
-        c = {  "email":associated_user.email,
-        	   'domain':'10.1.1.16:8000',
-        	   'site_name': 'Website',
-               "uid": urlsafe_base64_encode(force_bytes(associated_user.pk)),
-        	   "user": associated_user,
-        	   'token': default_token_generator.make_token(associated_user),
-        	   'protocol': 'http',
-    		}
-        print(c)
-        print(render_to_string(email_template_name, c))
+        associated_users = User.objects.filter(email=email)
+        print("associated_user", associated_users)
+        for user in associated_users:
+            if user.exists():
+                subject = "Password Reset Requested"
+                email_template_name = "../templates/password_reset_email_template.txt"
+                c = {  "email":user.email,
+                	   'domain':'10.1.1.16:8000',
+                	   'site_name': 'Website',
+                       "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                	   "user": user,
+                	   'token': default_token_generator.make_token(user),
+                	   'protocol': 'http',
+            		}
 
+                #Sending email
+                content = MIMEMultipart()
+                secure_data_loader = SecureDataLoader()
+                content["subject"] = subject
+                content["from"] = secure_data_loader.secure_data['SMTP_ACCOUNT']
+                content["to"] = user.email
+                content.attach(MIMEText(render_to_string(email_template_name, c)))
 
-        #Sending email
-        content = MIMEMultipart()
-        secure_data_loader = SecureDataLoader()
-        content["subject"] = subject
-        content["from"] = secure_data_loader.secure_data['SMTP_ACCOUNT']
-        content["to"] = associated_user.email
-        content.attach(MIMEText(render_to_string(email_template_name, c)))
-
-        with smtplib.SMTP(host="smtp.gmail.com", port="587") as smtp:
-            try:
-                smtp.ehlo()
-                smtp.starttls()
-                smtp.login( secure_data_loader.secure_data['SMTP_ACCOUNT'],  secure_data_loader.secure_data['SMTP_PASSWORD'])
-                smtp.send_message(content)
-                print("Complete!")
-            except Exception as e:
-                print("Error message: ", e)
+                with smtplib.SMTP(host="smtp.gmail.com", port="587") as smtp:
+                    try:
+                        smtp.ehlo()
+                        smtp.starttls()
+                        smtp.login( secure_data_loader.secure_data['SMTP_ACCOUNT'],  secure_data_loader.secure_data['SMTP_PASSWORD'])
+                        smtp.send_message(content)
+                        print("Complete!")
+                    except Exception as e:
+                        print("Error message: ", e)
 
     context = {
 
