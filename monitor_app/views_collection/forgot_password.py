d9dbb3f0a12b0dee0e119ced7143a68e0637c483
ArthurWuTW <lion811004@gmail.com>
2020-10-28 21:09:48 +0800

    forgot_password: Submit and assciate user

--- monitor_app/views_collection/forgot_password.py
+++ monitor_app/views_collection/forgot_password.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from ..models import *
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.models import User
 
 def forgot_password(request):
+    if(request.method == 'POST'):
+        email = request.POST.get('email', '')
+        print("email", email)
+        associated_user = User.objects.filter(email=email)
+        print("associated_user", associated_user)
+        if associated_user.exists():
+            #Sending email
+
+
+    # if post
+    #     gemerate token
+    #     send email
 
     context = {
 
