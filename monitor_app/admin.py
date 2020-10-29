f8676f3354973e10686e857339efe4b24bb610f1
ArthurWuTW <lion811004@gmail.com>
2020-10-30 02:12:40 +0800

    monitor_app: refactor admin.py

--- monitor_app/admin.py
+++ monitor_app/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import *
+import os
 
-admin.site.register(Temperature)
-admin.site.register(Humidity)
-admin.site.register(CpuTemperature)
-admin.site.register(TimePrice)
-admin.site.register(GrowthRate)
-admin.site.register(Profile)
-admin.site.register(AuthGroup)
+import glob
+from os.path import dirname, basename, join
+models_collection = [file for file in os.listdir(join(dirname(__file__), "models_collection")) if file.endswith(".py")]
+
+for f in models_collection:
+    import_script =\
+"""\
+admin.site.register({0})\
+""".format(f[:-3])
+    print(import_script)
+    exec (import_script)
