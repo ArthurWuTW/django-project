515f40775ab22cecd35609e40e39616d688d7add
ArthurWuTW <lion811004@gmail.com>
2020-10-30 16:31:13 +0800

    Add Plantdata model draft

---
+++ monitor_app/models_collection/PlantData.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class PlantData(models.Model):
+    aruco_id = models.IntegerField(blank=False, null=False, default=-1)
+    image_url = models.CharField(max_length=100, null=True)
+    type = models.CharField(max_length=25, null=True)
+    growth_rate = models.FloatField(null=True,
+                              blank=True,
+                              default=None)
+    seed_date = models.DateTimeField(null=True)
+    status = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.aruco_id)
+
+    class Meta:
+        verbose_name_plural = 'PlantData'
