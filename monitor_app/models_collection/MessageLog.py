4bff0a4f6fc87867174af43bb48cb365e7ec056d
ArthurWuTW <lion811004@gmail.com>
2020-11-08 17:41:22 +0800

    messageLog: many to one - foreign key user

--- monitor_app/models_collection/MessageLog.py
+++ monitor_app/models_collection/MessageLog.py
@@ -1,14 +1,15 @@
 from django.db import models
 from .AuthGroup import AuthGroup
+from django.contrib.auth.models import User
 
 class MessageLog(models.Model):
-    group = models.OneToOneField(AuthGroup,
-                                 blank=True,
-                                 null=True,
-                                 on_delete=models.SET_NULL)
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             default="")
     time = models.DateTimeField()
     title = models.CharField(max_length=25)
     log = models.CharField(max_length=100)
+    read = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
