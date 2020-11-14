f7357d0e1773dc6611463c562e90068c2f7a092a
ArthurWuTW <lion811004@gmail.com>
2020-11-14 05:29:10 +0000

    Refactoring 4 TaskStatus Views

--- monitor_app/views_collection/updateCameraTask.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from ..models import *
-
-def updateCameraTask(request, status):
-    task = TaskStatus.objects.get(task_name="CAMERA TASK")
-    task.status = status
-    task.save()
-
-    return HttpResponse('succeed')
--- monitor_app/views_collection/updatePiCpuTemperature.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from ..models import *
-
-def updatePiCpuTemperature(request, status):
-    task = TaskStatus.objects.get(task_name="PI CPU TEMPERATURE")
-    task.status = status
-    task.save()
-
-    return HttpResponse('succeed')
