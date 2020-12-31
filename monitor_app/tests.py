982f7564179109115d2869fb040a4b5e958dbc68
ArthurWuTW <lion811004@gmail.com>
2020-12-31 01:15:31 +0000

    Create tests collection

--- monitor_app/tests.py
+++ monitor_app/tests.py
@@ -1,5 +1,3 @@
-from django.test import TestCase
-
 # WARNING before running the script below, make sure the db account has the permission
 # to create database (because in testing, django creates a db and destroy it after testing)
 # > python3 manage.py test
@@ -8,39 +6,13 @@ from django.test import TestCase
 # > sudo -u postgres psql
 # > ALTER USER <django-db-account> CREATEDB;
 
-from django.urls import resolve
-from django.http import HttpRequest
-from django.test import RequestFactory
-from django.contrib.auth.models import AnonymousUser
-import monitor_app.views
-from monitor_app.views_collection.handlers.PiCpuTempStatusHander import PiCpuTempStatusHander
-from monitor_app.views_collection.handlers.WateringStatusHander import WateringStatusHander
-from monitor_app.views_collection.handlers.CameraTaskStatusHander import CameraTaskStatusHander
-from monitor_app.views_collection.handlers.WarningStatusHander import WarningStatusHander
-
-# test url resolves to dashboard view
-class DashboardPageTest(TestCase):
-    def test_root_url_resolves_to_dashboard_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func.view_class, monitor_app.views_collection.Dashboard.Dashboard)
-
-    def test_dashboard_html_with_fake_data(self):
-        piTempHandler = PiCpuTempStatusHander()
-        piTempHandler.create_fake_data("1000c")
-        wateringHandler = WateringStatusHander()
-        wateringHandler.create_fake_data("DDone")
-        cameraTaskHandler = CameraTaskStatusHander()
-        cameraTaskHandler.create_fake_data("87%")
-        warningHandler = WarningStatusHander()
-        warningHandler.create_fake_data("1000")
-
-        request = RequestFactory().get('/')
-        request.user = AnonymousUser()
-        view = monitor_app.views_collection.Dashboard.Dashboard.as_view()
-        response = view(request)
-
-        self.assertIn(b'<span>1000c</span></div>', response.content)
-        self.assertIn(b'<span>DDone</span></div>', response.content)
-        self.assertIn(b'<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">87%</div>', response.content)
-        self.assertIn(b'<div class="h5 mb-0 font-weight-bold text-gray-800">1000</div>', response.content)
-        self.assertTrue(response.content.strip().endswith(b'</html>'))
+import glob
+from os.path import dirname, basename, join
+tests_collection = glob.glob(join(dirname(__file__), "tests_collection", "*.py"))
+for f in tests_collection:
+    import_script =\
+"""\
+from .{0}.{1} import *\
+""".format("tests_collection", basename(f[:-3]).replace('/', '.'))
+    # print(import_script)
+    exec (import_script)
