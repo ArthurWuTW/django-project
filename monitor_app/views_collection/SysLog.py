2180232b2d72d6eda6483b19eaa1f9f3211083e7
ArthurWuTW <lion811004@gmail.com>
2021-02-08 06:04:57 +0000

    SysLog use cache instead

--- monitor_app/views_collection/SysLog.py
+++ monitor_app/views_collection/SysLog.py
@@ -5,6 +5,7 @@ import json
 from django.contrib.auth.models import User
 from django.db import connection
 from django.views import View
+from django.core.cache import cache
 
 import glob
 from os.path import dirname, basename, join
@@ -26,36 +27,42 @@ class SysLog(View):
         connHandler.setTitle("connections_data")
         connHandler.setTimezoneShift(timedelta(hours=8))
         connHandler.setThresholdTimestamp(threshold_timestamp)
+        connHandler.useCacheData(cache)
 
         connHandler_private_server = ConnectionHandler()
         connHandler_private_server.setQueryServerName("PrivateServer")
         connHandler_private_server.setTitle("connections_data_private_server")
         connHandler_private_server.setTimezoneShift(timedelta(hours=8))
         connHandler_private_server.setThresholdTimestamp(threshold_timestamp)
+        connHandler_private_server.useCacheData(cache)
 
         connHandler_backup_cpu = ConnectionHandler()
         connHandler_backup_cpu.setQueryServerName("BackupCpuPercentage")
         connHandler_backup_cpu.setTitle("connections_data_backup_cpu")
         connHandler_backup_cpu.setTimezoneShift(timedelta(hours=8))
         connHandler_backup_cpu.setThresholdTimestamp(threshold_timestamp)
+        connHandler_backup_cpu.useCacheData(cache)
 
         connHandler_backup_mem = ConnectionHandler()
         connHandler_backup_mem.setQueryServerName("BackupMemPercentage")
         connHandler_backup_mem.setTitle("connections_data_backup_mem")
         connHandler_backup_mem.setTimezoneShift(timedelta(hours=8))
         connHandler_backup_mem.setThresholdTimestamp(threshold_timestamp)
+        connHandler_backup_mem.useCacheData(cache)
 
         connHandler_webserver_cpu = ConnectionHandler()
         connHandler_webserver_cpu.setQueryServerName("WebServerCpuPercentage")
         connHandler_webserver_cpu.setTitle("connections_data_webserver_cpu")
         connHandler_webserver_cpu.setTimezoneShift(timedelta(hours=8))
         connHandler_webserver_cpu.setThresholdTimestamp(threshold_timestamp)
+        connHandler_webserver_cpu.useCacheData(cache)
 
         connHandler_webserver_mem = ConnectionHandler()
         connHandler_webserver_mem.setQueryServerName("WebServerMemPercentage")
         connHandler_webserver_mem.setTitle("connections_data_webserver_mem")
         connHandler_webserver_mem.setTimezoneShift(timedelta(hours=8))
         connHandler_webserver_mem.setThresholdTimestamp(threshold_timestamp)
+        connHandler_webserver_mem.useCacheData(cache)
 
 
 
